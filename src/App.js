893605b6d2c9c49d5a654e4dc8eba7d936b8f0d8
Ben Weber <ben.weberj@gmail.com>
2020-05-29 12:33:27 -0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+import * as V from 'victory'
+
+const Root = styled.div`
+  background: #5c1015;
+`
+
+function App() {
+  return (
+    <Root>
+
+    </Root>
+  )
+}
+
+export default App
